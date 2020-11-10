b9075edf7e04268ad3c794d5c54ef600687a126a
Robert Mayer <rwlmayer@gmail.com>
2020-11-10 10:31:33 +0100

    added LimitedList

---
+++ ConsoleApp2/ConsoleApp2/Models/LimitedList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2.Models
+{
+    public class LimitedList<T>
+    {
+        private int _maxNumberOfItems;
+        private List<T> _list;
+
+        public LimitedList(int maxNumberOfItems)
+        {
+            _maxNumberOfItems = maxNumberOfItems;
+            _list = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            if (_list.Count < _maxNumberOfItems)
+            {
+                _list.Add(item);
+            }
+        }
+
+        public void Remove(T item)
+        {
+            _list.Remove(item);
+        }
+
+        public int Count()
+        {
+            return _list.Count;
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using ConsoleApp2.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            LimitedList<Person> myLimitedList = new LimitedList<Person>(7);
+
+            LimitedList<string> ll = new LimitedList<string>(10);
+            ll.Add("hello world!");
+            ll.Add("Robert");
+            ll.Add("har");
+            ll.Add("en");
+            ll.Add("torktumlare");
+            ll.Add("som");
+            ll.Add("fungerar");
+
+            // en lista som innehåller max 5 items.
+            // Robert: kommer det alltid vara 5 items? JA!
+
+            // LINQ
+
+            Console.WriteLine("Listan innehåller " + ll.Count() + " items");
+        }
+    }
+}
